b078bbd70cf680b973a5389fda69e8867ff8874b
Gabriela Batista Pauka <gabrielabpauka@hotmail.com>
2024-04-16 21:53:38 -0300

    feat(usuario): CRUD usuario

--- src/br/com/autenticacao/Main.java
+++ src/br/com/autenticacao/Main.java
@@ -1,10 +1,21 @@
 package br.com.autenticacao;
 
+import br.com.autenticacao.controller.UsuarioController;
+import br.com.autenticacao.model.Usuario;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		UsuarioController controller = new UsuarioController();
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome("Harry");
+		usuario.setEmail("harry@proway.com");
+		usuario.setSenha("Harry123");
+		usuario.setIsAtivo(true);
+		
+		controller.cadastrar(usuario);
 	}
 
 }
